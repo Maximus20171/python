3684f7879f6230f268a4754ef0f81af9b12c2e4e
Mironov Maxim <maximus2017@mail.ru>
2020-02-24 16:17:20 +0500

    new file lesson2_6

--- Lesson2/Lesson2_6.py
+++ Lesson2/Lesson2_6.py
@@ -1,29 +1,19 @@
 kol= int(input('Введите кол-во товара '))
-my_list = []
-my_tuple = ()
+my_list= []
 my_dict = {}
-my_dict2 = {}
+my_anakitics = {'название': [], 'цена': [], 'количество': [], 'eд': []}
 i =0
 while i<kol:
     i+=1
     print(f'{i}-й товар:')
-    my_dict['название'] = input("название ")
-    my_dict['цена'] = int(input("цена "))
-    my_dict['количество'] = int(input("количество "))
-    my_dict['ед'] = input("ед ")
-    print(my_dict)
-    #my_dict2.update(i: my_dict)
-    a=str(my_dict)
-    print(a)
-    my_dict2.update({i: a})
-    print(my_dict2)
-    #my_tuple += (i, a)
-    #print(my_tuple)
-    #my_list.append(my_tuple)
-    #my_list.append(my_tuple[i])
-    #my_list.append(i)
-    #my_list.append(my_dict)
-    #my_tuple=tuple(my_dict,)
-    #my_tuple=tuple(str(my_list[i-0]+my_list[]))
-print(my_list)
-print(my_dict2.items())
+    my_dict ={'название':input("название "), 'цена':int(input("цена ")), 'количество':int(input("количество ")), 'eд': input("ед ")}
+    for f in my_anakitics.keys():
+        my_anakitics[f].append(my_dict[f])
+    my_list.append((i, my_dict))
+
+for t in my_list:
+    print(t)
+
+c = input('по какому пункту сделать аналитику? (название/цена/количество/ед) ')
+print(f'по ключу {c}: ', my_anakitics.get(c)) if my_anakitics.get(c) else print('Нет такого парраметра')
+
