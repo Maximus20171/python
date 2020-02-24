8c60d7104a27619a9e029f2a3009d64129bb0fec
Mironov Maxim <maximus2017@mail.ru>
2020-02-24 21:48:21 +0500

    new file lesson2_1

--- Lesson2/Lesson2_1.py
+++ Lesson2/Lesson2_1.py
@@ -1,2 +1,3 @@
-my_list = [1, 'slovo', 2.4, {1, 4, 5}, {'kl1': 1, 'kl2': 2, 'kl3': 3}]
-print(type(my_list))
+my_list = [1, 'slovo', 2.4, {1, 4, 5}, {'kl1': 1, 'kl2': 2, 'kl3': 3}, True, None]
+for f in my_list:
+    print(type(f))
